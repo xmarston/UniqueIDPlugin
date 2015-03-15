f1a9d6c294dc355cda3d137c2ab7caa13d4ed8ad
Ricardo Cámara <rcamara9@gmail.com>
2015-03-15 15:31:34 +0100

    Added Java Class for Android
    Still missing properties for Android in plugin.xml

---
+++ src/android/UniqueIDPlugin.java
@@ -0,0 +1,66 @@
+package es.dev-coders.UniqueIDPlugin;
+
+import android.content.Context;
+import android.provider.Settings.Secure;
+import android.telephony.TelephonyManager
+
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaInterface;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.provider.Settings;
+
+public class UniqueIDPlugin extends CordovaPlugin {
+
+	 /**
+     * Constructor.
+     */
+    public UniqueIDPlugin() {
+    }
+
+    /**
+     * Sets the context of the Command. This can then be used to do things like
+     * get file paths associated with the Activity.
+     *
+     * @param cordova The context of the main Activity.
+     * @param webView The CordovaWebView Cordova is running in.
+     */
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+    }
+
+     /**
+     * Executes the request and returns PluginResult.
+     *
+     * @param action            The action to execute.
+     * @param args              JSONArry of arguments for the plugin.
+     * @param callbackContext   The callback id used when calling back into JavaScript.
+     * @return                  True if the action was valid, false if not.
+     */
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+         if (action.equals("getUniqueID")) {
+            String id = "";
+            try {
+                id = this.getId();
+            } catch (JSONException e) { }
+
+            PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, id);
+            pluginResult.setKeepCallback(true);
+            callbackContext.sendPluginResult(pluginResult);
+            return true;
+        }
+        return false;
+    }
+
+	public String getId() {
+    	Context context = cordova.getActivity().getApplicationContext();
+    	TelephonyManager  manager = (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);
+    	String uuid = manager.getDeviceId();
+    	
+    	return uuid;
+    }
+}
